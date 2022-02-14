54cde84373427fdb51b2d214c4c9e6c32a8f980d
DANIELLEE <sky11213@hanmail.net>
2022-02-15 00:32:56 +0900

    React Source Files

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Hello from './Hello.js'
+
+function App(){
+  return (
+    <div>
+      <Hello name="react" color="blue" />
+      <Hello color="blue" />\
+    </div>
+  );
+
+}
+
+export default App;
